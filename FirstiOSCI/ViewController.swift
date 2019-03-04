0184bf0c7ba906f8fe039e9aafb215410689188b
AKantTest <anatoliyvarmii@gmail.com>
2019-03-04 13:14:38 +0300

    Initial Commit

---
+++ FirstiOSCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstiOSCI
+//
+//  Created by CI_Server on 04/03/2019.
+//  Copyright © 2019 CI_Server. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
